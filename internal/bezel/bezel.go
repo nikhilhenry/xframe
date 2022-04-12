ac63be73c8f3d99b154831f3da3b8e3e1ef03646
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-04-12 09:29:17 +0530

    :recycle: create bezel struct

---
+++ internal/bezel/bezel.go
@@ -0,0 +1,50 @@
+package bezel
+
+import (
+	"bytes"
+	"embed"
+	"fmt"
+	"image"
+	"log"
+)
+
+// Iphone enum of supported bezels
+const (
+	Iphone13Pro string = "iphone-13-pro.png"
+)
+
+//go:embed assets
+var assetsFs embed.FS
+
+// Bezel holds the image, bound and name of a iphone device bezel
+type Bezel struct {
+	Image  *image.Image
+	Bounds image.Rectangle
+	Name   string
+}
+
+// New returns a Bezel for a given device name.
+// Default device is Iphone 13 Pro
+func New(name string) *Bezel {
+	switch name {
+	case Iphone13Pro:
+		err, img := load(Iphone13Pro)
+		if err != nil {
+			log.Fatalln("failed to load image")
+		}
+		// todo write better dereferencing
+		bounds := *img
+		return &Bezel{Image: img, Bounds: bounds.Bounds(), Name: name}
+	default:
+		return &Bezel{}
+	}
+}
+
+func load(name string) (error, *image.Image) {
+	deviceImageFile, err := assetsFs.ReadFile(fmt.Sprintf("assets/%v", name))
+	deviceImage, _, err := image.Decode(bytes.NewReader(deviceImageFile))
+	if err != nil {
+		return err, nil
+	}
+	return nil, &deviceImage
+}
