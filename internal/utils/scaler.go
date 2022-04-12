962ce6e2dcd2c74871873a1b8aa29bd730a17c4f
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-04-12 22:34:16 +0530

    :necktie: switch to ApproxBiLinear scaler

--- internal/utils/scaler.go
+++ internal/utils/scaler.go
@@ -8,7 +8,7 @@ import (
 func EncodeWithScale(dim Dimension, encode func(rgba *image.RGBA) error) func(img *image.RGBA) error {
 	scaledDstImage := image.NewRGBA(image.Rect(0, 0, dim.Width, dim.Height))
 	return func(img *image.RGBA) error {
-		draw.NearestNeighbor.Scale(scaledDstImage, scaledDstImage.Bounds(), img, img.Bounds(), draw.Over, nil)
+		draw.ApproxBiLinear.Scale(scaledDstImage, scaledDstImage.Bounds(), img, img.Bounds(), draw.Over, nil)
 		err := encode(scaledDstImage)
 		if err != nil {
 			return err
