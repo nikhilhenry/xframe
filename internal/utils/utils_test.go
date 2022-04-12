feea5a84387e5c6a78aee9fd6d3c3724a91cd078
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-04-12 21:17:00 +0530

    :test_tube: define requirements for dim flag function

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -2,7 +2,22 @@ package utils
 
 import "testing"
 
-func Test_getFilePath(t *testing.T) {
+func TestGetDimensionsFromFlag(t *testing.T) {
+	t.Run("it returns width and height from string", func(t *testing.T) {
+		const dimFlag = "1920x1080"
+		want := struct {
+			Width  int
+			Height int
+		}{1920, 1080}
+		got := GetDimensionsFromFlag(dimFlag)
+
+		if got != want {
+			t.Errorf("expected '%q' but got '%q'", want, got)
+		}
+	})
+}
+
+func TestGetFilePath(t *testing.T) {
 
 	t.Run("it returns output with input image name when '.'", func(t *testing.T) {
 		const imagePath = "./frame/testdata/screen-home.png"
