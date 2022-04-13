ea3cb7f8b1f4f082c796cc59b0adac449b3a4c02
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-04-13 21:43:35 +0530

    :test_tube: write spec for sequence generation as gif

---
+++ pkg/frame/frame_sequence.go
@@ -0,0 +1,9 @@
+package frame
+
+import "image"
+
+// @todo accept array of image.Image instead of image.Palleted
+
+func GenerateSequence(encode func([]*image.Paletted) error, overlay overlay, screenImage []*image.Paletted) error {
+	return nil
+}
---
+++ pkg/frame/frame_sequence_test.go
@@ -0,0 +1,30 @@
+package frame_test
+
+import (
+	"bytes"
+	approvals "github.com/approvals/go-approval-tests"
+	"github.com/nikhilhenry/xframe/internal/bezel"
+	"github.com/nikhilhenry/xframe/internal/utils"
+	"github.com/nikhilhenry/xframe/pkg/frame"
+	_ "image/gif"
+	"testing"
+)
+
+func TestGenerateSequence(t *testing.T) {
+	approvals.UseFolder("testdata")
+	var screenshot = getScreenTestGIF(t)
+	deviceBezel := bezel.Bezel{Name: bezel.Iphone13Pro}
+	t.Run("it generates a gif with the device bezel", func(t *testing.T) {
+		imgs := utils.DecodeGIF(screenshot)
+
+		buf := bytes.Buffer{}
+		err := frame.GenerateSequence(utils.EncodeGIF(&buf, screenshot.Delay), deviceBezel, imgs)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		reader := bytes.NewReader(buf.Bytes())
+		approvals.VerifyWithExtension(t, reader, ".gif")
+	})
+}
