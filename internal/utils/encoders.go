87c61c1f2ee35d5a864660b9c2e3d97e5701628a
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-04-13 21:17:31 +0530

    :sparkles: create a GIF encoder

--- internal/utils/encoders.go
+++ internal/utils/encoders.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"image"
+	"image/gif"
 	"image/png"
 	"io"
 )
@@ -9,6 +10,16 @@ import (
 func EncodePNG(w io.Writer) func(*image.RGBA) error {
 	return func(drawableImg *image.RGBA) error {
 		if err := png.Encode(w, drawableImg); err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func EncodeGIF(w io.Writer, delay []int) func([]*image.Paletted) error {
+	return func(imgs []*image.Paletted) error {
+		processedGIF := gif.GIF{Image: imgs, Delay: delay}
+		if err := gif.EncodeAll(w, &processedGIF); err != nil {
 			return err
 		}
 		return nil
