240eb897454f8b73bc44b737df61aaa175befaaf
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-04-14 15:17:48 +0530

    :test_tube: create spec test

--- internal/video/video_test.go
+++ internal/video/video_test.go
@@ -1,6 +1,9 @@
 package video
 
 import (
+	"github.com/nikhilhenry/xframe/internal/utils"
+	"image/gif"
+	"os"
 	"testing"
 )
 
@@ -10,4 +13,31 @@ func TestDecode(t *testing.T) {
 	if err != nil {
 		t.Error(t)
 	}
+}
+
+func TestEncode(t *testing.T) {
+	var screenshot = getScreenTestGIF(t)
+	imgs := utils.DecodeGIF(screenshot)
+	err := Encode("./output/video-framed.mp4", imgs)
+	if err != nil {
+		t.Error(err)
+	}
+}
+func getScreenTestGIF(t testing.TB) gif.GIF {
+	t.Helper()
+	reader, err := os.Open("../../pkg/frame/testdata/screen-home.gif")
+	defer func(reader *os.File) {
+		err := reader.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}(reader)
+	if err != nil {
+		t.Error(err)
+	}
+	screenShotImage, err := gif.DecodeAll(reader)
+	if err != nil {
+		t.Error("unable to decode gif file")
+	}
+	return *screenShotImage
 }
