85918417be8c187db4b5ef2f1441194966e855eb
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-04-14 16:25:30 +0530

    :sparkles: produce images!

--- internal/video/encoder.go
+++ internal/video/encoder.go
@@ -5,15 +5,18 @@ import (
 	"github.com/nikhilhenry/xframe/internal/utils"
 	"image"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"sync"
 )
+
+var encodeFileTemplate = "img-out-temp%v.png"
 
 func Encode(videoPath string, imgs []image.Image) error {
 	// make a temp dir to store encoded images
 	dirName, err := os.MkdirTemp("", "xframe-frame-store")
 	fmt.Println(dirName)
-	//defer os.RemoveAll(dirName)
+	defer os.RemoveAll(dirName)
 	if err != nil {
 		return err
 	}
@@ -24,7 +27,7 @@ func Encode(videoPath string, imgs []image.Image) error {
 		go func(index int, rgba image.Image) {
 			defer wg.Done()
 			// make a new image
-			outputPath, err := os.Create(filepath.Join(dirName, fmt.Sprintf("img-out-temp%v.png", index)))
+			outputPath, err := os.Create(filepath.Join(dirName, fmt.Sprintf(encodeFileTemplate, index)))
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -35,5 +38,25 @@ func Encode(videoPath string, imgs []image.Image) error {
 		}(i, img)
 	}
 	wg.Wait()
+	// now pass these images to ffmpeg and encode
+	cmd := exec.Command("ffmpeg", "-y",
+		"-framerate", "64",
+		"-f", "image2",
+		"-i", filepath.Join(dirName, fmt.Sprintf(encodeFileTemplate, "%d")),
+		"-vcodec", "libx264",
+		"-pix_fmt", "yuv420p",
+		filepath.Join(videoPath),
+	)
+	cmd.Stderr = os.Stderr // bind log stream to stderr
+
+	// start process on another goroutine
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	// wait for ffmpeg to finish
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+
 	return nil
 }
