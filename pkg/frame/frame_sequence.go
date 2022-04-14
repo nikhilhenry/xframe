5d695a8170c90abe0c294b2637ec532a8ef3985c
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-04-14 08:58:32 +0530

    :white_check_mark: implement without encoding

--- pkg/frame/frame_sequence.go
+++ pkg/frame/frame_sequence.go
@@ -1,18 +1,18 @@
 package frame
 
 import (
-	"bytes"
-	"github.com/nikhilhenry/xframe/internal/utils"
 	"golang.org/x/image/draw"
 	"image"
-	"image/color/palette"
 )
 
-// @todo accept array of image.Image instead of image.Paletted
+type result struct {
+	index int
+	image image.Image
+}
 
-func GenerateSequence(encode func([]*image.Paletted) error, overlay overlay, screenImages []*image.Paletted) error {
+func GenerateSequence(encode func([]image.Image) error, overlay overlay, screenImages []*image.Image) error {
 	// temporary array with palette images [store the processed buffers]
-	framedImages := make([]*image.Paletted, len(screenImages))
+	framedImages := make([]image.Image, len(screenImages))
 	// channel to support concurrency
 	resultsChannel := make(chan result)
 
@@ -20,23 +20,17 @@ func GenerateSequence(encode func([]*image.Paletted) error, overlay overlay, scr
 	const imageHeight = 2532
 
 	for index, img := range screenImages {
-		go func(i int, imageFrame *image.Paletted) error {
+		go func(i int, imageFrame *image.Image) error {
 
-			imageBuf := bytes.Buffer{}
 			scaledDstImage := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
-			draw.NearestNeighbor.Scale(scaledDstImage, scaledDstImage.Bounds(), imageFrame, imageFrame.Bounds(), draw.Over, nil)
-			err := Generate(utils.EncodePNG(&imageBuf), overlay, scaledDstImage)
+			draw.NearestNeighbor.Scale(scaledDstImage, scaledDstImage.Bounds(), *imageFrame, (*imageFrame).Bounds(), draw.Over, nil)
+			err := Generate(func(rgba *image.RGBA) error {
+				resultsChannel <- result{index: i, image: rgba}
+				return nil
+			}, overlay, scaledDstImage)
 			if err != nil {
 				return err
 			}
-			//@todo palette image without png encoding for fast performance
-			// convert to palette image for processing
-			framedImage, _, err := image.Decode(&imageBuf)
-			if err != nil {
-				return err
-			}
-			resultsChannel <- result{index: i, image: imageToPaletted(framedImage, palette.Plan9)}
-
 			return nil
 		}(index, img)
 	}
